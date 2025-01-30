2392308cacfd64a46342751c4e6f988990fcd5f2
伊欧 <langyo.china@gmail.com>
2025-01-30 12:53:45 +0800

    🔧 修改镜像上传位置。

--- upload.ps1
+++
@@ -1,6 +0,0 @@
-zola build
-docker build -t homepage .
-
-# 这部分指令只有我本人能执行，其他人如有需要，请自行换源
-docker tag homepage:latest registry.cn-shanghai.aliyuncs.com/langyo/homepage:latest
-docker push registry.cn-shanghai.aliyuncs.com/langyo/homepage:latest
